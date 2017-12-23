0aa6e941964591153a3bbb13cca057d956e8044e
Chris Underwood <cunderwoodmn@gmail.com>
2017-12-22 19:23:27 -0800

    add find array method with additional single line option commented out.

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ console.log({allAdults});
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const comment = comments.find(function(comment){
+  if(comment.id === 823423) {
+    return true;
+  }
+});
+
+console.log(comment);
+
+//convert to arrow function, one line, with implicit return
+//const comment = comments.find(comment => comment.id === 823423);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
